388e7eb3840d304bb01beb13792329e7e5085052
user name <email@domain.com>
2021-05-17 23:08:56 +0400

    practice1

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const express = require("express");
-
-const todoModel = require("./schema");
-
 const db = require("./db");
-
-
+const todoModel = require("./schema");
 const app = express();
 app.use(express.json());
 
+
 app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+
+app.post("/create/todo", (req, res) => {
+
+  const { task , description, deadline, isCompleted, priority } = req.body;
+
+  const toDo = new todoModel({ task , description, deadline, isCompleted, priority });
+
+  toDo
+      .save()
+      .then((result1) => {
+        res.json(result1);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+
+});
+
+
+
+
+
+
+
+
+
+
 app.put("/update/todo", (req, res) => {});
 app.delete("/delete/todo", (req, res) => {});
 
 
-const toDo = new todoModel({
-  task,
-  description,
- deadline,
-  isCompleted,
-  priority,
-});
 
-// using promisees to save a toDo
+
+
+
+
+
+
+
+
+
+
+
 
 
 
